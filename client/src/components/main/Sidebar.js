b54499e4fe042f7a29f93d9afd6abc4a0764a649
gt <geuryt@yahoo.com>
2023-03-24 07:55:46 -0400

    code formatting fix, title fix

--- client/src/components/main/Sidebar.js
+++ client/src/components/main/Sidebar.js
@@ -1,176 +1,305 @@
-import { Box, Button, FormControl, FormLabel, Input, Heading, Select, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Heading,
+  Select,
+  VStack,
+} from '@chakra-ui/react';
 import React, { useContext, useEffect } from 'react';
 import { useFormik } from 'formik';
-import { useToast } from '@chakra-ui/react'
+import { useToast } from '@chakra-ui/react';
 import { AppContext } from '../../AppContext';
 
-
 export default function Sidebar() {
+  const { appData, setAppData } = useContext(AppContext);
+  const toast = useToast();
+  const formik = useFormik({
+    initialValues: {
+      host: '',
+      username: '',
+      password: '',
+      port: '',
+      transport: '',
+      sshPort: '',
+    },
+    onSubmit: (values, { setSubmitting }) => {
+      fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: values.username,
+          secretkey: values.password,
+          host: values.host,
+          port: values.port,
+          transport: values.transport,
+          sshPort: values.sshPort,
+        }),
+      })
+        .then(response => {
+          if (response.status === 200) {
+            toast({
+              title: `Login Successful`,
+              status: 'success',
+              duration: 20000,
+              position: 'bottom-right',
+              isClosable: true,
+            });
+          } else {
+            toast({
+              title: `Login Failed`,
+              status: 'error',
+              duration: 20000,
+              position: 'bottom-right',
+              isClosable: true,
+            });
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          toast({
+            title: `Login to ${values.host} Failed`,
+            status: 'error',
+            duration: 20000,
+            position: 'bottom-right',
+            isClosable: true,
+          });
+        })
+        .finally(() => setSubmitting(false));
+    },
+  });
 
-    const { appData, setAppData } = useContext(AppContext);
-    const toast = useToast()
-    const formik = useFormik({
-        initialValues: {
-            host: '',
-            username: '',
-            password: '',
-            port: '',
-            transport: '',
-            sshPort: '',
-        },
-        onSubmit: (values, {setSubmitting }) => {
-            fetch('/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: values.username,
-                    secretkey: values.password,
-                    host: values.host,
-                    port: values.port,
-                    transport: values.transport,
-                    sshPort: values.sshPort,
-                }),
-            })
-            .then(response => {
-                if (response.status === 200) {
-                    toast({
-                        title: `Login Successful`,
-                        status: 'success',
-                        duration: 20000,
-                        position: 'bottom-right',
-                        isClosable: true,
-                    });
-                }
-                else {
-                    toast({
-                        title: `Login Failed`,
-                        status: 'error',
-                        duration: 20000,
-                        position: 'bottom-right',
-                        isClosable: true,
-                    });
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                toast({
-                    title: `Login to ${values.host} Failed`,
-                    status: 'error',
-                    duration: 20000,
-                    position: 'bottom-right',
-                    isClosable: true,
-                });
-            })
-            .finally(() => setSubmitting(false));
-        },
+  useEffect(() => {
+    setAppData({
+      ...appData,
+      problemCategory: 'problemcategory',
+      problemType: 'problemtype',
     });
+  }, []);
 
-    useEffect(() => {
-        setAppData({
-            ...appData,
-            problemCategory: 'problemcategory',
-            problemType: 'problemtype'
-        })
-    }, []  );
-    
+  function handleProblemCategory(e) {
+    setAppData({
+      ...appData,
+      problemCategory: e.target.value,
+      problemType: 'problemtype',
+    });
+  }
 
-    function handleProblemCategory(e) {
-        setAppData({
-            ...appData,
-            problemCategory: e.target.value,
-            problemType: 'problemtype'
-        })
-    }
+  function handleProblemType(e) {
+    setAppData({
+      ...appData,
+      problemType: e.target.value,
+    });
+  }
 
-    function handleProblemType(e) {
-        setAppData({
-            ...appData,
-            problemType: e.target.value,
-        })
-        
-    }
-  
-    return (
-        <Box maxW='480px'>
-        <Heading as='h1' color='white' size='xl'>FortiGPT 🤖</Heading>
-        <br />
-        
-        <FormControl as="form"  mb='20px' maxW='300px' onSubmit={formik.handleSubmit} isRequired>
-          <FormLabel id='host' color={'white'}>Device IP/Hostname:</FormLabel>
-          <Input id='host' placeholder='IP/Hostname' color='white' type='text' name='host' value={formik.values.host} onChange={formik.handleChange} />
+  return (
+    <Box maxW="480px">
+      <Heading as="h1" color="white" size="xl">
+        FortiGPT 🤖
+      </Heading>
+      <br />
 
-          <FormLabel id='username' paddingTop='5px' color={'white'}>Username:</FormLabel>
-          <Input id='username' placeholder='Username' color='white' type='text' name='username' value={formik.values.username} onChange={formik.handleChange} />
-  
-          <FormLabel id='password' paddingTop='5px'color={'white'}>Password:</FormLabel>
-          <Input id='password' placeholder='Password' color='white' type='password' name='password' value={formik.values.password} onChange={formik.handleChange} />
+      <FormControl
+        as="form"
+        mb="20px"
+        maxW="300px"
+        onSubmit={formik.handleSubmit}
+        isRequired
+      >
+        <FormLabel id="host" color={'white'}>
+          Device IP/Hostname:
+        </FormLabel>
+        <Input
+          id="host"
+          placeholder="IP/Hostname"
+          color="white"
+          type="text"
+          name="host"
+          value={formik.values.host}
+          onChange={formik.handleChange}
+        />
 
-          <FormLabel id='transport' paddingTop='5px' color={'white'}>HTTP or HTTPS:</FormLabel>
-          <Input id='transport' placeholder='HTTP or HTTPS' color='white' type='text' name='transport' value={formik.values.transport} onChange={formik.handleChange} />
-  
-          <FormLabel id='port' paddingTop='5px' color={'white'}>HTTP/HTTPS Port:</FormLabel>
-          <Input id='port' placeholder='Enter HTTP or HTTPS Port' color='white' type='text' name='port' value={formik.values.port} onChange={formik.handleChange} />
+        <FormLabel id="username" paddingTop="5px" color={'white'}>
+          Username:
+        </FormLabel>
+        <Input
+          id="username"
+          placeholder="Username"
+          color="white"
+          type="text"
+          name="username"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+        />
 
-          <FormLabel id='sshPort' paddingTop='5px' color={'white'}>SSH Port:</FormLabel>
-          <Input id='sshPort' placeholder='Enter SSH Port' color='white' type='text' name='sshPort' value={formik.values.sshPort} onChange={formik.handleChange} />
-  
-          <Button borderRadius="full" mt='20px' colorScheme="blue" type="submit" isLoading={formik.isSubmitting} isDisabled={formik.isSubmitting}>Login to Fortigate</Button>
-        </FormControl>
-        
-        <VStack maxW='300px' spacing={4} >
-        <Select bg='#202324' pt='5px' color='white' onChange={handleProblemCategory}>
-            <option style={{ background: '#202324' }} value='problemcategory'>Problem Category</option>
-            <option style={{ background: '#202324' }} value='performance'>Performance</option>
-            <option style={{ background: '#202324' }} value='connectivity'>Connectivity</option>
-            <option style={{ background: '#202324' }} value='routing'>Routing</option>
-            <option style={{ background: '#202324' }} value='vpn'>VPN</option>
-            
+        <FormLabel id="password" paddingTop="5px" color={'white'}>
+          Password:
+        </FormLabel>
+        <Input
+          id="password"
+          placeholder="Password"
+          color="white"
+          type="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+
+        <FormLabel id="transport" paddingTop="5px" color={'white'}>
+          HTTP or HTTPS:
+        </FormLabel>
+        <Input
+          id="transport"
+          placeholder="HTTP or HTTPS"
+          color="white"
+          type="text"
+          name="transport"
+          value={formik.values.transport}
+          onChange={formik.handleChange}
+        />
+
+        <FormLabel id="port" paddingTop="5px" color={'white'}>
+          HTTP/HTTPS Port:
+        </FormLabel>
+        <Input
+          id="port"
+          placeholder="Enter HTTP or HTTPS Port"
+          color="white"
+          type="text"
+          name="port"
+          value={formik.values.port}
+          onChange={formik.handleChange}
+        />
+
+        <FormLabel id="sshPort" paddingTop="5px" color={'white'}>
+          SSH Port:
+        </FormLabel>
+        <Input
+          id="sshPort"
+          placeholder="Enter SSH Port"
+          color="white"
+          type="text"
+          name="sshPort"
+          value={formik.values.sshPort}
+          onChange={formik.handleChange}
+        />
+
+        <Button
+          borderRadius="full"
+          mt="20px"
+          colorScheme="blue"
+          type="submit"
+          isLoading={formik.isSubmitting}
+          isDisabled={formik.isSubmitting}
+        >
+          Login to Fortigate
+        </Button>
+      </FormControl>
+
+      <VStack maxW="300px" spacing={4}>
+        <Select
+          bg="#202324"
+          pt="5px"
+          color="white"
+          onChange={handleProblemCategory}
+        >
+          <option style={{ background: '#202324' }} value="problemcategory">
+            Problem Category
+          </option>
+          <option style={{ background: '#202324' }} value="performance">
+            Performance
+          </option>
+          <option style={{ background: '#202324' }} value="connectivity">
+            Connectivity
+          </option>
+          <option style={{ background: '#202324' }} value="routing">
+            Routing
+          </option>
+          <option style={{ background: '#202324' }} value="vpn">
+            VPN
+          </option>
         </Select>
-        
+
         {appData.problemCategory === 'performance' && (
-            <>
-                <Select py='20px' onChange={handleProblemType} bg='#202324' pt='5px' color='white' >
-                <option style={{ background: '#202324' }} value='problemtype'>Problem Type</option>
-                <option style={{ background: '#202324' }} value='highcpu'>High CPU</option>
-                <option style={{ background: '#202324' }} value='highmemory'>High Memory</option>
-                </Select>
-            </>
-            )}
+          <>
+            <Select
+              py="20px"
+              onChange={handleProblemType}
+              bg="#202324"
+              pt="5px"
+              color="white"
+            >
+              <option style={{ background: '#202324' }} value="problemtype">
+                Problem Type
+              </option>
+              <option style={{ background: '#202324' }} value="highcpu">
+                High CPU
+              </option>
+              <option style={{ background: '#202324' }} value="highmemory">
+                High Memory
+              </option>
+            </Select>
+          </>
+        )}
 
         {appData.problemCategory === 'connectivity' && (
-            <>
-                <Select onChange={handleProblemType} bg='#202324' pt='5px' color='white' >
-                <option style={{ background: '#202324' }} value='problemtype'>Problem Type</option>
-                <option style={{ background: '#202324' }} value='packetflow'>Packet Flow</option>
-                </Select>
-            </>
-            )}
+          <>
+            <Select
+              onChange={handleProblemType}
+              bg="#202324"
+              pt="5px"
+              color="white"
+            >
+              <option style={{ background: '#202324' }} value="problemtype">
+                Problem Type
+              </option>
+              <option style={{ background: '#202324' }} value="packetflow">
+                Packet Flow
+              </option>
+            </Select>
+          </>
+        )}
 
         {appData.problemCategory === 'routing' && (
-            <>
-                <Select onChange={handleProblemType} bg='#202324' pt='5px' color='white' >
-                <option style={{ background: '#202324' }} value='problemtype'>Problem Type</option>
-                <option style={{ background: '#202324' }} value='bgp'>BGP Down</option>
-                </Select>
-            </>
-            )}
+          <>
+            <Select
+              onChange={handleProblemType}
+              bg="#202324"
+              pt="5px"
+              color="white"
+            >
+              <option style={{ background: '#202324' }} value="problemtype">
+                Problem Type
+              </option>
+              <option style={{ background: '#202324' }} value="bgp">
+                BGP Down
+              </option>
+            </Select>
+          </>
+        )}
 
         {appData.problemCategory === 'vpn' && (
-            <>
-                <Select onChange={handleProblemType} bg='#202324' pt='5px' color='white' >
-                <option style={{ background: '#202324' }} value='problemtype'>Problem Type</option>
-                <option style={{ background: '#202324' }} value='vpndown'>VPN Down</option>
-                </Select>
-            </>
-            )}
-
-            
-        </VStack>
-
-        
-        </Box>
-    );
-    }
-
+          <>
+            <Select
+              onChange={handleProblemType}
+              bg="#202324"
+              pt="5px"
+              color="white"
+            >
+              <option style={{ background: '#202324' }} value="problemtype">
+                Problem Type
+              </option>
+              <option style={{ background: '#202324' }} value="vpndown">
+                VPN Down
+              </option>
+            </Select>
+          </>
+        )}
+      </VStack>
+    </Box>
+  );
+}
--- client/src/components/performance/Highcpu.js
+++ client/src/components/performance/Highcpu.js
@@ -1,105 +1,99 @@
-import React, {useState, useEffect} from 'react'
-import { Button, HStack, Text, Textarea, } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import { Button, HStack, Text, Textarea } from '@chakra-ui/react';
 import Disclaimer from '../main/Disclaimer';
 import Chatgpt from '../chatgpt/Chatgpt';
 
-
 export default function Highcpu() {
+  const [debugOutput, setDebugOutput] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [ChatGptPrompt, setChatGptPrompt] = useState('');
+  const [documentationLink, setDocumentationLink] = useState('');
 
-    
-    const [debugOutput, setDebugOutput] = useState("");
-    const [loading, setLoading] = useState(false); 
-    const [ChatGptPrompt, setChatGptPrompt] = useState("");
-    const [documentationLink, setDocumentationLink] = useState("");
+  function handleClick() {
+    // Run the high cpu debug script
+    setLoading(true);
+    setDebugOutput('');
+    fetch('/performance/high_cpu_script', {
+      method: 'POST',
+    })
+      .then(response => response.text())
+      .then(data => {
+        setDebugOutput(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setDebugOutput('Try Logging in again');
+        setLoading(false);
+      });
+  }
 
+  useEffect(() => {
+    // Get the chatgpt prompt
+    fetch('/chatgpt_prompts/performance/high_cpu.txt')
+      .then(response => response.text())
+      .then(text => {
+        setChatGptPrompt(text);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
-
-
-    function handleClick() {
-
-        // Run the high cpu debug script
-        setLoading(true);
-        setDebugOutput("");
-        fetch("/performance/high_cpu_script", {
-            method: "POST",
-        })
-          .then((response) => response.text())
-          .then((data) => {
-            setDebugOutput(data);
-            setLoading(false);
-          })
-          .catch((error) => {
-            console.error(error);
-            setDebugOutput('Try Logging in again');
-            setLoading(false);
-          })
-    }
-
-    useEffect(() => {
-       // Get the chatgpt prompt
-        fetch('/chatgpt_prompts/performance/high_cpu.txt')
-          .then(response => response.text())
-          .then(text => {
-            setChatGptPrompt(text);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }, []);
-    
-      useEffect(() => {
-        // Get the documentation link
-        fetch('/chatgpt_prompts/performance/high_cpu_link.txt')
-          .then(response => response.text())
-          .then(text => {
-            setDocumentationLink(text);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }, []);
+  useEffect(() => {
+    // Get the documentation link
+    fetch('/chatgpt_prompts/performance/high_cpu_link.txt')
+      .then(response => response.text())
+      .then(text => {
+        setDocumentationLink(text);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <>
+      <Text p="10px" fontSize={15} color="white">
+        👉 Instructions: Click Start Debug to gather High CPU Debugs, once
+        complete click on Run FortiGPT 👈
+      </Text>
 
-    <Text p='10px' fontSize={15} color="white">👉 Instructions: Click Start Debug to gather High CPU Debugs, once complete click on Run FortiGPT 👈</Text>
-
-
-    <HStack>
-            <Text p='15px' color="white" fontWeight="bold">✨ DEBUG OUTPUT ✨</Text>
-            <Button 
-          borderRadius="full"  
-          colorScheme="blue" 
-          size="md" 
-          ml={4} 
-          isLoading={loading} 
+      <HStack>
+        <Text p="15px" color="white" fontWeight="bold">
+          ✨ DEBUG OUTPUT ✨
+        </Text>
+        <Button
+          borderRadius="full"
+          colorScheme="blue"
+          size="md"
+          ml={4}
+          isLoading={loading}
           onClick={handleClick}
-          loadingText='Start Debug'
+          loadingText="Start Debug"
         >
           Start Debug
         </Button>
-    </HStack>
+      </HStack>
 
+      <Textarea
+        color="white"
+        bg="gray.800"
+        defaultValue={debugOutput}
+        my={4}
+        size="md"
+        maxWidth="2xl"
+      />
 
+      <>
+        <Disclaimer documentationLink={documentationLink} />
+      </>
 
-    <Textarea
-          color='white'
-          bg='gray.800'
-          defaultValue={debugOutput}
-          my={4}
-          size="md"
-          maxWidth='2xl'
-    />
-
-        <>
-        <Disclaimer documentationLink={documentationLink}/>
-        </>
-
-        <>
-        {debugOutput && <Chatgpt debugOutput={debugOutput} chatGptPrompt={ChatGptPrompt} />}
-        </>
-
-
+      <>
+        {debugOutput && (
+          <Chatgpt debugOutput={debugOutput} chatGptPrompt={ChatGptPrompt} />
+        )}
+      </>
     </>
   );
 }
